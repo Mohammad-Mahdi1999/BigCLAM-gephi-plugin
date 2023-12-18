7119755989db34eb9539a701dcf73061568ce050
M. Mahdi Farahbakhsh <mahdi.farahbakhsh99@gmail.com>
2023-12-18 21:25:59 +0330

    UI created

---
+++ modules/BigCLAMGephiPlugin/src/main/java/BigCLAMUI.java
@@ -0,0 +1,65 @@
+import org.gephi.statistics.spi.Statistics;
+import org.gephi.statistics.spi.StatisticsUI;
+import org.openide.util.lookup.ServiceProvider;
+
+import javax.swing.*;
+
+@ServiceProvider(service = StatisticsUI.class)
+public class BigCLAMUI implements StatisticsUI {
+
+    private BigCLAMPluginPanel panel;
+    private BigCLAMPlugin myCliqueDetector;
+
+    @Override
+    public JPanel getSettingsPanel() {
+        panel = new BigCLAMPluginPanel();
+        return panel;
+    }
+
+    @Override
+    public void setup(Statistics ststcs) {
+        this.myCliqueDetector = (BigCLAMPlugin) ststcs;
+        if (panel != null) {
+            panel.setK(myCliqueDetector.getK());
+        }
+    }
+
+    @Override
+    public void unsetup() {
+        if (panel != null) {
+            myCliqueDetector.setK(panel.getK());
+        }
+        panel = null;
+    }
+
+    @Override
+    public Class<? extends Statistics> getStatisticsClass() {
+        return BigCLAMPlugin.class;
+    }
+
+    @Override
+    public String getValue() {
+        return null;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "BigCLAM";
+    }
+
+    @Override
+    public String getShortDescription() {
+        return "BigCLAM implementation in gephi";
+    }
+
+    @Override
+    public String getCategory() {
+        return CATEGORY_NETWORK_OVERVIEW;
+    }
+
+    @Override
+    public int getPosition() {
+        return 800;
+    }
+
+}
