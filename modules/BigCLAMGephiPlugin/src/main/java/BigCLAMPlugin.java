029079b441322f32db9de04c8da6328df2831411
M. Mahdi Farahbakhsh <mahdi.farahbakhsh99@gmail.com>
2023-12-18 23:11:34 +0330

    panel updated

--- modules/BigCLAMGephiPlugin/src/main/java/BigCLAMPlugin.java
+++ modules/BigCLAMGephiPlugin/src/main/java/BigCLAMPlugin.java
@@ -7,9 +7,7 @@
 //import org.gephi.desktop.api.WorkspaceProvider;
 //import org.gephi.desktop.project.api.ProjectControllerUI;
 
-import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.GraphModel;
-import org.gephi.graph.api.Node;
 import org.gephi.utils.longtask.spi.LongTask;
 import org.gephi.utils.progress.ProgressTicket;
 
@@ -25,150 +23,60 @@ public class BigCLAMPlugin implements org.gephi.statistics.spi.Statistics, LongT
     private int numCommunities;
     private double[][] memberships;
 
-    //    public BigCLAMPlugin( int numNodes, int numCommunities){
-//        this.numNodes = numNodes;
-//        this.numCommunities = numCommunities;
-//        this.memberships = new double[numNodes][numCommunities];
-//    }
-    //
-//    private ProgressTicket progressTicket;
-//    private int k = 0;
-//    private Set<Set<Node>> Cliques = new HashSet<Set<Node>>();
-//    GenQueue<TreeSet<Node>> Bk = new GenQueue<TreeSet<Node>>();
-//
-//    public class SortByID implements Comparator<Node> {
-//
-//        public int compare(Node n1, Node n2) {
-//            if (n1.getStoreId() > n2.getStoreId()) {
-//                return 1;
-//            } else {
-//                return -1;
-//            }
-//        }
-//    }
-//
-//    //<editor-fold defaultstate="collapsed" desc="Queue Implementation">
-//    public Object getLastElement(final Collection c) {
-//        /*
-//         final Iterator itr = c.iterator();
-//         Object lastElement = itr.next();
-//         while (itr.hasNext()) {
-//         lastElement = itr.next();
-//         }
-//         return lastElement;
-//         */
-//        return null;
-//    }
-//
-//    class GenQueue<E> {
-//
-//        private LinkedList<E> list = new LinkedList<E>();
-//
-//        public void enqueue(E item) {
-//            list.addLast(item);
-//        }
-//
-//        public E dequeue() {
-//            return list.pollFirst();
-//        }
-//
-//        public boolean hasItems() {
-//            return !list.isEmpty();
-//        }
-//
-//        public int size() {
-//            return list.size();
-//        }
-//
-//        public void addItems(GenQueue<? extends E> q) {
-//            while (q.hasItems()) {
-//                list.addLast(q.dequeue());
-//            }
-//        }
-//    }
-//    //</editor-fold>
-//
-//    private Vector<Node> getLargerIndexNodes(Graph g, Node vi) {
-//        Vector<Node> output = new Vector<Node>();
-//        for (Node n : g.getNodes()) {
-//            if (n.getStoreId() > vi.getStoreId() && g.isAdjacent(n, vi)) {
-//                //TODO check degree of n and vi
-//                output.addElement(n);
-//            }
-//        }
-//
-//        return output;
-//    }
-//
-//    private boolean checkBk1IsClique(Graph g, TreeSet<Node> Bk1) {
-//        for (Node firstNode : Bk1) {
-//            for (Node secondNode : Bk1) {
-//                if (firstNode == secondNode) {
-//                    continue;
-//                }
-//
-//                if (!(g.isAdjacent(firstNode, secondNode) )) { //One edge is missing in the Bk+1 clique
-//                    return false;
-//                }
-//            }
-//        }
-//
-//        return true;
-//    }
-//
-//    Random r = new Random();
-//
-    @Override
-    public void execute(GraphModel gm) {
-        Graph g = gm.getGraphVisible();
-
-        g.readLock();
-
-        if (g.isDirected()) {
-            report = "Graph cannot be directed";
-            return;
-        }
-//
-        //Firstly add each node as an item in Bk
-
-
-        //Algorithm finished.
-        //Write the output
-//        report += "Clique Detection started. Nodes with <b>" + (k - 1) + "</b> edges will not be included.";
-//        report += "<br><br>";
-//        report += "Found Cliques of size " + getK() + ".<br>Now making new graph ...<br>Clearing old graph ...";
+        public BigCLAMPlugin(){
+        this.numNodes = 1;
+        this.numCommunities = 1;
+        this.memberships = new double[numNodes][numCommunities];
     }
 
-    private int getSharedNodes(Node vi, Node vj) {
-        String[] firstCliqueNodes = vi.getLabel().split(",");
-        String[] secondCliqueNodes = vj.getLabel().split(",");
+    private ProgressTicket progressTicket;
 
-        int sharedNodes = 0;
+    @Override
+    public void execute(GraphModel gm) {
 
-        for (String n1 : firstCliqueNodes) {
-            for (String n2 : secondCliqueNodes) {
-                if (n1.equals(n2)) {
-                    sharedNodes++;
-                }
+        this.memberships = new double[numNodes][numCommunities];
+
+        int numNodes = 10;
+        int numCommunities = 2;
+        int[][] adjacencyMatrix = new int[numNodes][numNodes];
+
+        // Initialize the adjacency matrix (this is just an example, you need to provide your own graph data)
+        for (int i = 0; i < numNodes; i++) {
+            for (int j = 0; j < numNodes; j++) {
+                adjacencyMatrix[i][j] = (int) (Math.random() + 0.5); // Binary graph for simplicity
             }
         }
 
-        return sharedNodes;
-    }
+        BigCLAMPlugin BigCLAMPlugin = new BigCLAMPlugin();//(numNodes, numCommunities);
+        BigCLAMPlugin.initializeMemberships();
+        BigCLAMPlugin.optimizeObjectiveFunction(adjacencyMatrix, 0.01, 100);
 
+        // Print the final community memberships
+        for (int i = 0; i < numNodes; i++) {
+            report = "Node " + i + " Memberships: " + Arrays.toString(BigCLAMPlugin.memberships[i]);
+        }
+    }
 
     @Override
     public void setProgressTicket(ProgressTicket pt) {
-//        this.progressTicket = pt;
+        this.progressTicket = pt;
     }
 //
-//    public int getK() {
-//        return k;
-//    }
-//
-//    public void setK(int k) {
-//        this.k = k;
-//    }
+    public int getNumNodes() {
+        return numNodes;
+    }
+
+    public void setNumNodes(int k) {
+        this.numNodes = numNodes;
+    }
+
+    public int getNumCommunities() {
+        return numCommunities;
+    }
+
+    public void setNumCommunities(int numCommunities) {
+        this.numCommunities = numCommunities;
+    }
 
 
     // Initialize memberships randomly
@@ -214,28 +122,6 @@ public class BigCLAMPlugin implements org.gephi.statistics.spi.Statistics, LongT
         return dotProduct;
     }
 
-    // Example usage
-    public static void main(String[] args) {
-        int numNodes = 10;
-        int numCommunities = 2;
-        int[][] adjacencyMatrix = new int[numNodes][numNodes];
-
-        // Initialize the adjacency matrix (this is just an example, you need to provide your own graph data)
-        for (int i = 0; i < numNodes; i++) {
-            for (int j = 0; j < numNodes; j++) {
-                adjacencyMatrix[i][j] = (int) (Math.random() + 0.5); // Binary graph for simplicity
-            }
-        }
-
-        BigCLAMPlugin BigCLAMPlugin = new BigCLAMPlugin();//(numNodes, numCommunities);
-        BigCLAMPlugin.initializeMemberships();
-        BigCLAMPlugin.optimizeObjectiveFunction(adjacencyMatrix, 0.01, 100);
-
-        // Print the final community memberships
-        for (int i = 0; i < numNodes; i++) {
-            System.out.println("Node " + i + " Memberships: " + Arrays.toString(BigCLAMPlugin.memberships[i]));
-        }
-    }
 
     @Override
     public String getReport() {
--- modules/BigCLAMGephiPlugin/src/main/java/BigCLAMPluginPanel.java
+++ modules/BigCLAMGephiPlugin/src/main/java/BigCLAMPluginPanel.java
@@ -1,68 +1,59 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class BigCLAMPluginPanel extends JPanel {
 
-    JTextField kvalue;
+    private JTextField textField1;
+    private JTextField textField2;
 
-    @SuppressWarnings("unchecked")
     public BigCLAMPluginPanel() {
-        //this.setLayout(null);
-        JLabel jXHeader1 = new JLabel();
-
-        jXHeader1.setText("Enter the value for k (clique size, ex: k = 3 will find triangualrs). Higher values of k may take more time for computation. This algorithm is NP-Hard, so use it carefully."); // NOI18N
-
-        JLabel label = new JLabel("Enter value of k here:");
-        label.setFont(new Font("Times New Roman", Font.ROMAN_BASELINE, 13));
-        this.add(label);
-        kvalue = new JTextField();
-        this.add(kvalue);
-        Insets insets = this.getInsets();
-
-        Dimension size = label.getPreferredSize();
-        label.setBounds(20 + insets.left, 30 + insets.top, size.width, size.height);
-
-        Dimension size1 = kvalue.getPreferredSize();
-        kvalue.setBounds(20 + insets.left, 130 + insets.top, size1.width + 20, size1.height);
-
-        javax.swing.GroupLayout layout = new GroupLayout(this);
-        this.setLayout(layout);
-
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jXHeader1, javax.swing.GroupLayout.DEFAULT_SIZE, 536, Short.MAX_VALUE)
-                        .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(label)
-                                .addContainerGap(354, Short.MAX_VALUE))
-                        .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(kvalue)
-                                .addContainerGap(382, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addComponent(jXHeader1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(label)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(kvalue)
-                                .addContainerGap(187, Short.MAX_VALUE))
-        );
+        initializeUI();
     }
 
-    public int getK() {
-        int i = 0;
-        try {
-            i = Integer.valueOf(kvalue.getText());
-        } catch (Exception ex) {
-            return 0;
-        }
-        return i;
+    private void initializeUI() {
+        setLayout(new GridLayout(3, 2));
+
+        JLabel label1 = new JLabel("number of Nodes:");
+        textField1 = new JTextField();
+        JLabel label2 = new JLabel("number of Communities:");
+        textField2 = new JTextField();
+
+        add(label1);
+        add(textField1);
+        add(label2);
+        add(textField2);
+
+        JButton okButton = new JButton("OK");
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+        add(okButton);
     }
 
-    public void setK(int k) {
-        this.kvalue.setText(String.valueOf(k));
+    private void onOK() {
+        String numNodes = textField1.getText();
+        String numCommunities = textField2.getText();
+
+        // Use the values as needed (e.g., print or process them)
+        System.out.println("number of Nodes: " + numNodes);
+        System.out.println("number of Communities: " + numCommunities);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("BigCLAMPluginPanel Test");
+                BigCLAMPluginPanel panel = new BigCLAMPluginPanel();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.getContentPane().add(panel);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
     }
 }
